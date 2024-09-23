d86d79245a2b0edac67f424c887911e1a4295e8f
Therendal <therendal@gmail.com>
2024-09-23 21:25:20 +0200

    Game window working and closing

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 #Importin librarys
 import pygame
+from constants import *
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 def main():
+    pygame.init()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    screen.fill(000)
+    pygame.display.flip()
+    
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()

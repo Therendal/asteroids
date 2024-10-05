ba71048ede56010210385ea7e30bb5c4fc5e9083
Therendal <therendal@gmail.com>
2024-10-05 17:20:29 +0200

    Added function for drawing player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Importin librarys
 import pygame
 from constants import *
+from player import *
 
 
 
@@ -9,14 +10,24 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #Clear and fills the screen
         screen.fill(000)
+        
+        #Draws player
+        player.draw(screen)
+
+        #updates screen
         pygame.display.flip()
+        
+        #Regulates fps
         dt = clock.tick(60) / 1000
+        
 
     #print("Starting asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
